794988f6eb0f00d297911e7bfd43bda6f7b960f8
Ayaan Labba <ch21b021@smail.iitm.ac.in>
2025-03-19 01:00:23 +0530

    Update neural_network.py fixing momentum explosion

--- neural_network.py
+++ neural_network.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class NeuralNetwork:
     def __init__(self, input_size=1, hidden_layers=[1], output_size=1, activation="ReLU", weight_init="random", optimizer="sgd", 
-                 lr=0.1, momentum=0.9, beta = 0.9, beta1=0.9, beta2=0.999, epsilon=1e-8, weight_decay=0):
+                 lr=0.01, momentum=0.9, beta = 0.9, beta1=0.9, beta2=0.999, epsilon=1e-8, weight_decay=0):
         self.layers = [input_size] + hidden_layers + [output_size]
         self.activation = activation
         self.weights, self.biases = self.init_weights(weight_init)
@@ -31,10 +31,10 @@ class NeuralNetwork:
             input_dim, output_dim = int(self.layers[i]), int(self.layers[i+1])
             
             if weight_init == "random":
-                w = np.random.randn(output_dim, input_dim) * 0.01 # Small random values
+                w = np.random.rand(output_dim, input_dim) # Small random values
             
             elif weight_init == "Xavier":
-                w = np.random.randn(output_dim, input_dim) * np.sqrt(1 / input_dim)  # Xavier initialization
+                w = np.random.rand(output_dim, input_dim) * np.sqrt(1 / input_dim)  # Xavier initialization
 
             b = np.zeros((output_dim, 1))  # Initialize biases to zero
 
@@ -44,6 +44,7 @@ class NeuralNetwork:
         return weights, biases
 
     def activation_function(self, x, derivative=False):
+        #x = np.clip(x, -500, 500)
         if self.activation == "sigmoid":
             sig = 1 / (1 + np.exp(-x))
             return sig * (1 - sig) if derivative else sig
@@ -56,81 +57,113 @@ class NeuralNetwork:
             return 1 if derivative else x  # Identity function
 
     def forward(self, X):
+        if X.ndim == 1:
+            X = X.reshape(1, -1)
+        
         H, A = [X], []
         h = X
 
-        for w, b in zip(self.weights, self.biases):
+        for i, (w, b) in enumerate(zip(self.weights, self.biases)):
             a = np.dot(h, w.T) + b.T
-            h = self.activation_function(a) if w is not self.weights[-1] else self.softmax(a)
-            H.append(h)
+            #a = np.clip(a, -1e10, 1e10)
             A.append(a)
-        
+
+            if i < len(self.weights) - 1:
+                h = self.activation_function(a)
+            else:
+                # Apply softmax for the output layer
+                h = self.softmax(a)
+            
+            H.append(h)
+            
         return H, A
     
     def softmax(self, x):
-        exp_x = np.exp(x - np.max(x, axis=0, keepdims=True))
-        return exp_x / np.sum(exp_x, axis=0, keepdims=True)
-        #return np.exp(x)/np.sum(np.exp(x), axis=0, keepdims=True)
+        #x = np.clip(x, -500, 500)
+        exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+        return exp_x / np.sum(exp_x, axis=1, keepdims=True)
     
     def loss(self, Y_pred, Y_true):
         n = Y_true.shape[0]
-        loss = -np.sum(Y_true * np.log(Y_pred + 1e-9)) / n
+        #Y_pred = np.clip(Y_pred, self.epsilon, 1 - self.epsilon)
+        loss = -np.sum(Y_true * np.log(Y_pred + self.epsilon)) / n
         return loss
     
-    def gradients(self, H, A, Y_true, dw, db):
+    def gradients(self, H, A, Y_true):
         n = Y_true.shape[0]
-        da = -(Y_true - H[-1])  # Cross-entropy gradient w.r.t output layer
-
-        for i in reversed(range(len(self.weights))):
-            dw[i] = np.dot(da, H[i].T) / n  #+ self.weight_decay * self.weights[i]
-            db[i] = np.sum(da, axis=0, keepdims=True) / n
-            if i > 0:
-                dh = np.dot(da, self.weights[i])
-                da = dh * self.activation_function(A[i-1], derivative=True)
-
-        return dw, db
-
-    def gradients_nag(self, H, A, Y_true, u_w):
-        m = Y_true.shape[1]
         da = -(Y_true - H[-1])  # Cross-entropy gradient w.r.t output layer
         dw, db = [], []
 
         for i in reversed(range(len(self.weights))):
-            dw[i] = np.dot(da, H[i].T) / m  #+ self.weight_decay * self.weights[i]
-            db[i] = np.sum(da, axis=0, keepdims=True) / m
+            dw.insert(0, np.dot(H[i].T, da) / n) #+ self.weight_decay * self.weights[i]
+            db.insert(0, np.sum(da, axis=0, keepdims=True) / n)
             if i > 0:
-                dh = np.dot((self.weights[i] - u_w).T, da)
+                dh = np.dot(da, self.weights[i])
+                #dh = np.clip(dh, -1e10, 1e10)
                 da = dh * self.activation_function(A[i-1], derivative=True)
-        
-        return dw, dh
+                #print(np.max(A[i-1]))
+                #da = np.clip(da, -1e10, 1e10)
+
+        return {"dw": dw, "db": db}
+
+    def gradients_nag(self, H, A, Y_true):
+        n = Y_true.shape[0]
+        da = -(Y_true - H[-1])  # Cross-entropy gradient w.r.t output layer
+        dw, db = [], []
+
+        for i in reversed(range(len(self.weights))):
+            dw.insert(0, np.dot(H[i].T, da) / n) #+ self.weight_decay * self.weights[i]
+            db.insert(0, np.sum(da, axis=0, keepdims=True) / n)
+            if i > 0:
+                dh = np.dot(da, self.weights[i] - self.u_w[i])
+                #dh = np.clip(dh, -1e10, 1e10)
+                da = dh * self.activation_function(A[i-1], derivative=True)
+                #print(np.max(A[i-1]))
+                #da = np.clip(da, -1e10, 1e10)
+
+        return {"dw": dw, "db": db}
     
-    def gradient_descent(self, dw, db, grads_nag=None):
+    def gradient_descent(self, grads, grads_nag=None):
         for i in range(len(self.weights)):
             if self.optimizer == "sgd":
-                self.weights[i] -= self.lr * dw[i].T
-                self.biases[i] -= self.lr * db[i].T
+                self.weights[i] -= self.lr * grads["dw"][i].T
+                self.biases[i] -= self.lr * grads["db"][i].T
             
             elif self.optimizer in ["momentum", "nesterov"]:
-                if self.optimizer == "nesterov":
-                    self.u_w[i] += self.momentum * self.u_w[i] + grads_nag["dw"][i].T
-                    self.u_b[i] += self.momentum * self.u_b[i] + grads_nag["db"][i].T
+                # Use appropriate gradients based on optimizer
+                if self.optimizer == "nesterov" and grads_nag is not None:
+                    grad_w = grads_nag["dw"][i].T
+                    grad_b = grads_nag["db"][i].T
                 else:
-                    self.u_w[i] += self.momentum * self.u_w[i] + dw[i].T
-                    self.u_b[i] += self.momentum * self.u_b[i] + db[i].T
-                self.weights[i] -= self.lr * self.u_w[i]
-                self.biases[i] -= self.lr * self.u_b[i]
+                    grad_w = grads["dw"][i].T
+                    grad_b = grads["db"][i].T
+                
+                # Clip gradients to prevent explosion
+                grad_w = np.clip(grad_w, -5.0, 5.0)
+                grad_b = np.clip(grad_b, -5.0, 5.0)
+                
+                # Update velocities with decay
+                self.u_w[i] = self.momentum * self.u_w[i] + self.lr * grad_w
+                self.u_b[i] = self.momentum * self.u_b[i] + self.lr * grad_b
+                
+                # Clip velocities to prevent explosion
+                self.u_w[i] = np.clip(self.u_w[i], -25.0, 25.0)
+                self.u_b[i] = np.clip(self.u_b[i], -25.0, 25.0)
+                
+                self.weights[i] -= self.u_w[i]
+                self.biases[i] -= self.u_b[i]
             
             elif self.optimizer == "rmsprop":
-                self.v_w[i] = self.beta * self.v_w[i] + (1 - self.beta) * (dw[i] ** 2).T
-                self.v_b[i] = self.beta * self.v_b[i] + (1 - self.beta) * (db[i] ** 2).T
-                self.weights[i] -= self.lr * dw[i] / (np.sqrt(self.v_w[i]) + self.epsilon)
+                self.v_w[i] = self.beta * self.v_w[i] + (1 - self.beta) * (grads["dw"][i] ** 2).T
+                self.v_b[i] = self.beta * self.v_b[i] + (1 - self.beta) * (grads["db"][i] ** 2).T
+                self.weights[i] -= self.lr * grads["dw"][i] / (np.sqrt(self.v_w[i]) + self.epsilon)
                 self.biases[i] -= self.lr * grads["db"][i] / (np.sqrt(self.v_b[i]) + self.epsilon)
             
             elif self.optimizer in ["adam", "nadam"]:
-                self.u_w[i] = self.beta1 * self.u_w[i] + (1 - self.beta1) * dw[i].T
-                self.u_b[i] = self.beta1 * self.u_b[i] + (1 - self.beta1) * db[i].T
-                self.v_w[i] = self.beta2 * self.v_w[i] + (1 - self.beta2) * (dw[i] ** 2).T
-                self.v_b[i] = self.beta2 * self.v_b[i] + (1 - self.beta2) * (db[i] ** 2).T
+                self.u_w[i] = self.beta1 * self.u_w[i] + (1 - self.beta1) * grads["dw"][i].T
+                self.u_b[i] = self.beta1 * self.u_b[i] + (1 - self.beta1) * grads["db"][i].T
+                self.v_w[i] = self.beta2 * self.v_w[i] + (1 - self.beta2) * (grads["dw"][i] ** 2).T
+                self.v_b[i] = self.beta2 * self.v_b[i] + (1 - self.beta2) * (grads["db"][i] ** 2).T
 
                 m_w_corr = self.u_w[i] / (1 - self.beta1 ** self.t)
                 m_b_corr = self.u_b[i] / (1 - self.beta1 ** self.t)
@@ -138,8 +171,8 @@ class NeuralNetwork:
                 v_b_corr = self.v_b[i] / (1 - self.beta2 ** self.t)
 
                 if self.optimizer == "nadam":
-                    m_w_corr = (self.beta1 * m_w_corr + (1 - self.beta1) * dw[i]) / (1 - self.beta1 ** self.t)
-                    m_b_corr = (self.beta1 * m_b_corr + (1 - self.beta1) * db[i]) / (1 - self.beta1 ** self.t)
+                    m_w_corr = (self.beta1 * m_w_corr + (1 - self.beta1) * grads["dw"][i]).T / (1 - self.beta1 ** self.t)
+                    m_b_corr = (self.beta1 * m_b_corr + (1 - self.beta1) * grads["db"][i]).T / (1 - self.beta1 ** self.t)
 
                 self.weights[i] -= self.lr * m_w_corr / (np.sqrt(v_w_corr) + self.epsilon)
                 self.biases[i] -= self.lr * m_b_corr / (np.sqrt(v_b_corr) + self.epsilon)
@@ -149,18 +182,13 @@ class NeuralNetwork:
     def train(self, X_train, Y_train, epochs, batch_size, log_interval=1):
         n = X_train.shape[0]
         for epoch in range(epochs):
-            indices = np.random.permutation(n)
-            X_train, Y_train = X_train[indices], Y_train[indices]
-            dw, db = [np.zeros_like(w) for w in self.weights], [np.zeros_like(b) for b in self.biases]
-
             for i in range(0, n, batch_size):
-                X_batch, Y_batch = X_train[i:i+batch_size, :], Y_train[i:i+batch_size, :]
+                X_batch, Y_batch = X_train[i:i+batch_size], Y_train[i:i+batch_size]
                 H, A = self.forward(X_batch)
-                dwi, dbi = self.gradients(H, A, Y_batch, dw, db)
-                dw += dwi
-                db += dbi
-            
-            self.gradient_descent(dw, db)
+                if self.optimizer == "nesterov":
+                    grads_nag = self.gradients_nag(H, A, Y_batch)
+                grads = self.gradients(H, A, Y_batch)
+                self.gradient_descent(grads)
 
             if epoch % log_interval == 0:
                 Y_pred = self.predict(X_train)
